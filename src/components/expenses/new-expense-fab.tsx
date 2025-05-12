7017406850de9d93c102266ba4f87d6b5c7e3959
Julian Gödde <129423545+jgoedde@users.noreply.github.com>
2025-05-12 22:07:56 +0200

    Update new-expense-fab.tsx

--- src/components/expenses/new-expense-fab.tsx
+++ src/components/expenses/new-expense-fab.tsx
@@ -7,7 +7,7 @@ export function NewExpenseFAB() {
     return (
         <button
             className={
-                "bg-primary-container text-on-primary-container sticky left-full bottom-1 size-16 rounded-2xl shadow-lg"
+                "bg-primary-container text-on-primary-container sticky left-[95%] bottom-1 size-16 rounded-2xl shadow-lg"
             }
             onClick={() => {
                 route("/new");
