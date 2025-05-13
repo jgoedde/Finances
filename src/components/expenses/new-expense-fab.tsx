90adf4561700e856998d381530f774c56cdff533
Julian Gödde <goedde@tuta.io>
2025-05-13 18:20:31 +0200

    fix FAB

--- src/components/expenses/new-expense-fab.tsx
+++ src/components/expenses/new-expense-fab.tsx
@@ -5,15 +5,15 @@ export function NewExpenseFAB() {
     const [, route] = useLocation();
 
     return (
-        <div
+        <button
             className={
-                "bg-primary-container text-on-primary-container mb-3 mr-3 sticky left-full bottom-0 size-16 rounded-2xl shadow-lg"
+                "bg-primary-container text-on-primary-container sticky bottom-6 left-full mr-6 size-16 rounded-2xl shadow-lg"
             }
             onClick={() => {
                 route("/new");
             }}
         >
             <Banknote className={"mx-auto size-10 h-full"} />
-        </div>
+        </button>
     );
 }
