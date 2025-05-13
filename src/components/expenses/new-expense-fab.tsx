368297b6f195b39d7377808f902a57b7b62730a5
Julian Gödde <goedde@tuta.io>
2025-05-13 19:20:24 +0200

    Add ripple effect to New Expense FAB button

--- src/components/expenses/new-expense-fab.tsx
+++ src/components/expenses/new-expense-fab.tsx
@@ -1,16 +1,24 @@
 import { Banknote } from "lucide-react";
 import { useLocation } from "wouter";
+import { useRipple } from "@/hooks/use-ripple.ts";
 
 export function NewExpenseFAB() {
     const [, route] = useLocation();
+    const ripple = useRipple();
 
     return (
         <button
             className={
-                "bg-primary-container text-on-primary-container sticky bottom-6 left-full mr-6 size-16 shrink-0 rounded-2xl shadow-lg"
+                "ripple-container bg-primary-container text-on-primary-container sticky bottom-6 left-[87vw] size-16 shrink-0 -translate-x-1/2 rounded-2xl shadow-lg"
             }
-            onClick={() => {
-                route("/new");
+            data-ripple-color="bg-on-surface/10"
+            {...ripple}
+            onClick={(e) => {
+                ripple.onClick(e);
+
+                setTimeout(() => {
+                    route("/new");
+                }, 100);
             }}
         >
             <Banknote className={"mx-auto size-10 h-full"} />
