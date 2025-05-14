b3578210bfda5bc1bede6f16d46c96f4f9cc18a0
Julian Gödde <129423545+jgoedde@users.noreply.github.com>
2025-05-14 08:01:26 +0200

    Update expenses-page.tsx

--- src/components/expenses/expenses-page.tsx
+++ src/components/expenses/expenses-page.tsx
@@ -112,7 +112,8 @@ export const ExpensesPage = () => {
                     {...ripple}
                     onClick={(e) => {
                         ripple.onClick(e);
-                        route("/reporting");
+
+                        setTimeout(()=>route("/reporting"),200)
                     }}
                 >
                     <CardHeader className={"flex flex-col font-medium"}>
@@ -135,7 +136,7 @@ export const ExpensesPage = () => {
                     {...ripple}
                     onClick={(e) => {
                         ripple.onClick(e);
-                        route("/reporting");
+                        setTimeout(()=>route("/reporting"),200)
                     }}
                 >
                     <CardHeader className={"flex flex-col font-medium"}>
@@ -158,7 +159,7 @@ export const ExpensesPage = () => {
                     {...ripple}
                     onClick={(e) => {
                         ripple.onClick(e);
-                        route("/reporting");
+                        setTimeout(()=>route("/reporting"),200)
                     }}
                 >
                     <CardHeader className={"flex flex-col"}>
