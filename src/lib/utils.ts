7d0c3ff24d2cbd277870dc07d90f40f3aff5563a
Julian Gödde <129423545+jgoedde@users.noreply.github.com>
2025-05-14 09:16:16 +0200

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,35 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import type { IconName } from "lucide-react/dynamic";
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
-}
-
-export function zer0IconToLucideIcon(icon: string): IconName {
-    switch (icon) {
-        case "food-fork-drink": {
-            return "utensils";
-        }
-        case "cash": {
-            return "shopping-basket";
-        }
-        case "gift-outline": {
-            return "gift";
-        }
-        case "bandage": {
-            return "heart";
-        }
-        case "bag-suitcase": {
-            return "shirt";
-        }
-        case "youtube-gaming": {
-            return "joystick";
-        }
-        case "airplane": {
-            return "plane";
-        }
-        default:
-            return icon as IconName;
-    }
 }
