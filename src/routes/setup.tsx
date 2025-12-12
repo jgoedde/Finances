60da71047288264196f4de682e8c2c60714d7eab
Julian Gödde <juliangd02@gmail.com>
2025-12-12 16:52:56 +0100

    autofocus the pw input

--- src/routes/setup.tsx
+++ src/routes/setup.tsx
@@ -154,6 +154,7 @@ function RouteComponent() {
                                     "border-outline focus:border-primary mt-1 block h-10 w-full rounded-xs border px-4 py-2 text-sm shadow-sm focus:border-2 focus:ring-0 focus:outline-none"
                                 }
                                 autoComplete={"current-password"}
+                                autoFocus
                                 required
                             />
                         </div>
